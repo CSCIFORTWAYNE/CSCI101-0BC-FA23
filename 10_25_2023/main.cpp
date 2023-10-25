792b3de775f5341ebeed38e8610aaf78929caff0
Angela Venable <avenable@ivytech.edu>
2023-10-25 16:29:24 -0400

    final demo 10/25/2023

--- 10_25_2023/main.cpp
+++ 10_25_2023/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
     double billAmount = 0;
     double billAmountCpy = 0;
+    std::string name;
 
     billAmount = 38.95;
 
@@ -12,11 +14,16 @@ int main()
     const double TIP_RATE = .2; // tipRate = .2;
 
     billAmount = 45.92;
+    std::cout << "Enter the bill amount: ";
+    std::cin >> billAmount;
 
     int x = 7;
     int y = 2;
-    std::cout << x / y << std::endl;
+    std::cout << static_cast<double>(x) / y << std::endl;
     std::cout << x % y << std::endl;
+
+    double tip = billAmount * TIP_RATE;
+    std::cout << "The 20% tip for bill amount $" << billAmount << " is $" << tip << std::endl;
 
     return 0;
 }
