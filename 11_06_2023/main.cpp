b77820214d12ed7b36a0b592eda7673a05ee7106
Angela <avenable11@ivytech.edu>
2023-11-06 14:29:31 -0500

    up to lecture activity

---
+++ 11_06_2023/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+
+int main()
+{
+    /*     int myNum = 93;
+        int yourNum = 0;
+        std::cout << "Enter a number between 1 and 100: ";
+        std::cin >> yourNum;
+        std::cout << std::endl;
+
+        std::string day;
+        std::cout << "What day is it? ";
+        std::cin >> day;
+        std::cout << std::endl;
+        std::transform(day.begin(), day.end(), day.begin(), ::tolower);
+
+        if (myNum >= yourNum || day == "monday")
+        {
+
+            std::cout << "My number is bigger. I win!!!!" << std::endl;
+            std::cout << "I'm still in the true block" << std::endl;
+            day = "Friday";
+            myNum + yourNum;
+        }
+        else
+        {
+            std::cout << "Your number was bigger." << std::endl;
+        }
+        std::cout << "Thanks for playing." << std::endl;
+
+        /*  if (0)
+             std::cout << "if(0) is true" << std::endl;
+         else
+             std::cout << "if(0) is false" << std::endl;
+
+        int score = 0;
+        std::cout << "Enter a score: ";
+        std::cin >> score;
+        std::cout << std::endl;
+
+        if (score >= 90)
+        {
+            std::cout << "The grade is A." << std::endl;
+        }
+        else if (score >= 80)
+        {
+            std::cout << "The grade is B." << std::endl;
+        }
+        else if (score >= 70)
+        {
+            std::cout << "The grade is C." << std::endl;
+        }
+        else if (score >= 60)
+        {
+            std::cout << "The grade is D." << std::endl;
+        }
+        else
+            std::cout << "The grade is F." << std::endl;
+
+        bool x = 1.0 == 3.0 / 7.0 + 2.0 / 7.0 + 2.0 / 7.0;
+     */
+    // fix the block of code below for the lecture activity
+    int num;
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+    std::cout << std::endl;
+    if (0 <= num <= 10)
+        std::cout << num << " is within 0 and 10." << std::endl;
+    else
+        std::cout << num << " is not within 0 and 10." << std::endl;
+
+    return 0;
+}
